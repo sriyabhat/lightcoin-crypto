a717b59644741ad53c37fa534d90ca36837b40c8
Sriya Bhat <sriya.s.bhat@gmail.com>
2020-09-27 01:41:10 +0000

    Implemented Banking app

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
+  constructor(username){
+    this.username = username;
+    //this.balance = 0;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+  get balance() {
+    let sum = 0;
+    for(let item of this.transactions) {
+      sum += item.value;
+    }
+    return sum;
+  }
+
+}
+
+class Transaction {
+  constructor(amount,account) {
     this.amount = amount;
+    this.account = account;
+  }
+
+  get isAllowed() {
+    if ((this.account.balance + this.value) > 0 ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    if(this.isAllowed){
+      this.account.addTransaction(this);
+      return true;
+    } else {
+      console.log('Insufficient Funds');
+      return false;
+    }
+
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -(this.amount);
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
@@ -15,15 +66,32 @@ class Withdrawal {
 
 
 
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account('Sriya');
+console.log('Starting Balance:', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Attempting to deposit $120.');
+t1 = new Deposit(120, myAccount);
+console.log(t1.commit());
+//console.log('Transaction 2:', t1);
 
-console.log('Balance:', balance);
+console.log('Attempting to withdraw $150.25.');
+t2 = new Withdrawal(150.25,myAccount);
+console.log(t2.commit());
+//console.log('Transaction 1:', t2);
+
+console.log('Attempting to withdraw $9.99.');
+t3 = new Withdrawal(9.99, myAccount);
+console.log(t3.commit());
+//console.log('Transaction 2:', t3);
+
+//console.log(myAccount.transactions);
+
+console.log('Ending Balance:', myAccount.balance);
+
+
+
+
